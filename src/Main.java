1977e063dabe21a66a2897fa6cfbb81a3bbe2b8b
yocong <rnfaos77@naver.com>
2024-04-05 13:54:14 +0900

    # 주석처리

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,18 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    public static void main(String[] args) {
-        System.out.println(10 + 5);
-        System.out.println("안뇽?"); // 백틱 불가능
-
-        int month = 5;
-        int day = 5;
-        String anni = "어린이날";
-
-        // ln은 line을 뜻함. 줄바꿈 O
-        System.out.println(month + "월 " + day + "일은 " + anni + "입니다.");
-        // printf는 줄바꿈 X
-        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
-        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
-    }
-}
+////TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+//// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+//public class Main {
+//    public static void main(String[] args) {
+//        System.out.println(10 + 5);
+//        System.out.println("안뇽?"); // 백틱 불가능
+//
+//        int month = 5;
+//        int day = 5;
+//        String anni = "어린이날";
+//
+//        // ln은 line을 뜻함. 줄바꿈 O
+//        System.out.println(month + "월 " + day + "일은 " + anni + "입니다.");
+//        // printf는 줄바꿈 X
+//        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
+//        System.out.printf("%d월 %d일은 %s입니다.\n", month, day, anni);
+//    }
+//}
--- src/day01/Variable.java
+++ src/day01/Variable.java
@@ -1,36 +1,36 @@
-package day01;
-
-public class Variable {
-
-    static int n4 = 40;
-
-    public static void main(String[] args) {
-
-        System.out.println(n4);
-        int score = 78; // 정수
-        String userName = "홍길동"; // 문자열
-
-        int userName123;
-        int phoneNumber;
-
-        // 변수의 스코프 - 블록레벨 스코프
-        // 자바의 변수는 생성된 블록에서 블록이 종료되면 메모리에서 사라짐
-        int i = 100;
-        for (int j= 0; j < 5; j++) {
-            System.out.println(j);
-        } // end for
-
-        int n1 = 10;
-        if (true) {
-            System.out.println(n1);
-            int n2 = 20;
-
-            while(true) {
-                System.out.println(n2);
-                int n3 = 30;
-                break;
-            }
-        } // end if
-            System.out.println(n1); // 불가능
-    } // end main
-} // end class
+//package day01;
+//
+//public class Variable {
+//
+//    static int n4 = 40;
+//
+//    public static void main(String[] args) {
+//
+//        System.out.println(n4);
+//        int score = 78; // 정수
+//        String userName = "홍길동"; // 문자열
+//
+//        int userName123;
+//        int phoneNumber;
+//
+//        // 변수의 스코프 - 블록레벨 스코프
+//        // 자바의 변수는 생성된 블록에서 블록이 종료되면 메모리에서 사라짐
+//        int i = 100;
+//        for (int j= 0; j < 5; j++) {
+//            System.out.println(j);
+//        } // end for
+//
+//        int n1 = 10;
+//        if (true) {
+//            System.out.println(n1);
+//            int n2 = 20;
+//
+//            while(true) {
+//                System.out.println(n2);
+//                int n3 = 30;
+//                break;
+//            }
+//        } // end if
+//            System.out.println(n1); // 불가능
+//    } // end main
+//} // end class
