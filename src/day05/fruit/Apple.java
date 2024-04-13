a443a8f7ce351a66d6d830c390b131d18ff6845e
yocong <rnfaos77@naver.com>
2024-04-13 17:49:48 +0900

    # 패키지

---
+++ src/day05/fruit/Apple.java
@@ -0,0 +1,8 @@
+package day05.fruit;
+
+public class Apple {
+
+    public Apple() {
+        System.out.println("fruit패키지의 사과입니다.");
+    }
+}
---
+++ src/day05/juice/Apple.java
@@ -0,0 +1,8 @@
+package day05.juice;
+
+public class Apple {
+
+    public Apple() {
+        System.out.println("juice의 사과입니다.");
+    }
+}
