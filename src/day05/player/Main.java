196a7a4c81802522fbd69881d60295f224902489
yocong <rnfaos77@naver.com>
2024-04-14 14:46:16 +0900

    # this. 과 this()

---
+++ src/day05/player/Main.java
@@ -0,0 +1,20 @@
+package day05.player;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+//        Player parking = new Player("주차왕파킹");
+//        Player insultKing = new Player("욕설왕뻐킹");
+
+//        System.out.println("parking = " + parking);
+//        System.out.println("insultKing = " + insultKing);
+
+//        insultKing.attack(parking);
+//        insultKing.attack(parking);
+//        insultKing.attack(parking);
+
+        Player p1 = new Player("야미");
+        System.out.println("p1 = " + p1);
+    }
+}
