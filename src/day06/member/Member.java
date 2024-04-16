96b0f0d1f23adeb91aeb515f6d8c63de4399dfa7
yocong <rnfaos77@naver.com>
2024-04-16 10:18:52 +0900

    # 회원관리 시스템 - 회원정보 객체 설계 (회원 1명)

---
+++ src/day06/member/Member.java
@@ -0,0 +1,39 @@
+package day06.member;
+
+import java.time.LocalDate;
+
+// 역할: 회원정보 1개를 가지고 있어야 한다.
+public class Member {
+
+    int id; // 식별변호
+    String email;
+    String memberName;
+    String password;
+    String gender;
+    int age;
+    LocalDate regDate; // 회원가입일자
+
+    // 생성자: 회원정보가 생성될 때 초기 데이터를 처리
+    // -> 객체가 생성될 시점에 호출되는 함수로 객체의 초기화를 담당한다.
+    // 시스템이 생성하는 데이터: 회원번호, 회원가입일자
+    // 사용자가 제공하는 데이터: 이메일, 패스워드, 이름, 성별, 나이
+
+    Member(String email, String password, String memberName, String gender, int age) {
+        this.id = 1;
+        this.regDate = LocalDate.now(); // 현재 시간을 읽어서 처리
+
+        this.email = email;
+        this.password = password;
+        this.memberName = memberName;
+        this.gender = gender;
+        this.age = age;
+    }
+
+
+
+    // toString: 객체의 정보를 출력할 수 있음 (자동호출)
+    public String toString() {
+        return String.format("# 이름: %s, 이메일: %s, 나이: %d세, 성별: %s, 가입일: %s"
+                , this.memberName, this.email, this.age, this.gender, this.regDate);
+    }
+}
---
+++ src/day06/member/Test.java
@@ -0,0 +1,23 @@
+package day06.member;
+
+public class Test {
+
+    public static void main(String[] args) {
+        // Member 타입의 배열 생성
+        // Member 타입만 배열에 들어갈 수 있음
+        Member[] arr = new Member[5];
+
+        Member m1 = new Member("rnfaos77@naver.com","1234","김철수", "남성", 30);
+        Member m2 = new Member("awef@awef.com", "4141", "김영희", "여성", 13);
+
+        arr[0] = m1; // Member 타입
+        arr[1] = m2; // Member 타입
+        arr[2] = new Member("adawe@naew.com", "3134", "홍길동","여성", 15);
+
+        System.out.println("m1 = " + m1);
+        System.out.println("m2 = " + m2);
+
+        Member[] members = {m1, m2};
+        System.out.println("members = " + members);
+    }
+}
