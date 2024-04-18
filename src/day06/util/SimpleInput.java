30493b633b52850623bd661b30e569f73e8db521
yocong <rnfaos77@naver.com>
2024-04-18 21:13:21 +0900

    # 상수

--- src/day06/util/SimpleInput.java
+++ src/day06/util/SimpleInput.java
@@ -5,10 +5,14 @@ import java.util.Scanner;
 // 역할: 스캐너 입력을 간소화해주는 객체
 public class SimpleInput {
 
-    Scanner sc = new Scanner(System.in);
+    private static Scanner sc;
+
+    static {
+        sc = new Scanner(System.in);
+    }
 
     // 문자열 입력을 처리
-    public String input(String message) {
+    public static String input(String message) {
         System.out.print(message);
         return sc.nextLine();
     }
