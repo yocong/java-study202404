1250c68e850495bb7e64f7d799e7d975776e3bef
yocong <rnfaos77@naver.com>
2024-04-22 21:57:58 +0900

    # 예외처리 실습

--- src/day06/member/MemberView.java
+++ src/day06/member/MemberView.java
@@ -43,7 +43,16 @@ public class MemberView {
             gender = MemberConstants.FEMALE;
         }
 
-        int age = Integer.parseInt(si.input("- 나이: "));
+
+        int age = 0;
+        while (true) {
+            try {
+                age = Integer.parseInt(si.input("- 나이: "));
+                break;
+            } catch (NumberFormatException e) {
+                System.out.println("나이는 숫자로 입력하세요!");
+            }
+        }
 
         // 입력데이터를 기반으로 한명의 회원 객체를 생성
         Member newMember = new Member(email, password, name, gender, age);
