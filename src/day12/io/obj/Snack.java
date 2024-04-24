17d641e5e374f429d28f783bcb209700c8611d1c
yocong <rnfaos77@naver.com>
2024-04-24 23:59:23 +0900

    # 파일 입출력 - 객체를 파일에 저장할 때는 직렬화(Serializable) 필요

---
+++ src/day12/io/obj/Snack.java
@@ -0,0 +1,78 @@
+package day12.io.obj;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class Snack implements Serializable {
+
+    public enum Taste {
+        GOOD, BAD, SOSO
+    }
+
+    private String snackName;
+    private int year; // 출시연도
+    private int price; // 가격
+    private Taste taste;
+
+    public Snack(String snackName, int year, int price, Taste taste) {
+        this.snackName = snackName;
+        this.year = year;
+        this.price = price;
+        this.taste = taste;
+    }
+
+    public String getSnackName() {
+        return snackName;
+    }
+
+    public void setSnackName(String snackName) {
+        this.snackName = snackName;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
+    public void setYear(int year) {
+        this.year = year;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public Taste getTaste() {
+        return taste;
+    }
+
+    public void setTaste(Taste taste) {
+        this.taste = taste;
+    }
+
+    @Override
+    public String toString() {
+        return "Snack{" +
+                "snackName='" + snackName + '\'' +
+                ", year=" + year +
+                ", price=" + price +
+                ", taste=" + taste +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Snack snack = (Snack) o;
+        return year == snack.year && price == snack.price && Objects.equals(snackName, snack.snackName) && taste == snack.taste;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(snackName, year, price, taste);
+    }
+}
